bb1ac664ede200d96afd6b54df808befd3014b61
Raphael Borba <raphaelbsx@gmail.com>
2025-01-02 22:21:03 -0300

    Initial Configs, add zustand, eslint and shadcn ui.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
+// eslint.config.mjs
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+// Import the plugin
+import tailwindcss from "eslint-plugin-tailwindcss";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +13,26 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    ignores: ["**/node_modules/**", "**/.next/**"],
+    plugins: {
+      tailwindcss,
+    },
+    rules: {
+      "tailwindcss/classnames-order": "warn",
+      "react/prop-types": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" }
+      ],
+    },
+  },
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:tailwindcss/recommended"
+  ),
 ];
 
 export default eslintConfig;
